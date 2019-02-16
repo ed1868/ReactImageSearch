b0dbd4b597d706b22602d0ac6f914ce8caf1f184
Eddie Ruiz <ruizeduardo21@gmail.com>
2019-02-16 17:01:10 -0500

    folder structure set up for search bar

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import SearchBar from '../SearchBar/searchBar'
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <SearchBar />
+         
+      </div>
+    );
+  }
+}
+
+export default App;
