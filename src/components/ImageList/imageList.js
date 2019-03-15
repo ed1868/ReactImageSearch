deda70e6fe14fcadc9f5c312133b0758ba68b9fb
Eddie Ruiz <ruizeduardo21@gmail.com>
2019-03-15 09:27:06 -0400

    refactor of img and alt added

--- src/components/ImageList/imageList.js
+++ src/components/ImageList/imageList.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
+import './imageList.css'
 
 export default class imageList extends Component {
 
   render(props) {
     let images = this.props.images;
 
-    let imageList= images.map(image => {
-      let url = image.urls.full;
-      return <img src={url} />
+    let imageList= images.map(({description ,id, urls} )=> {
+      return <img className="column" alt={description} key={id} src={urls.regular} />
     });
     return (
       <div>
-        <div>{imageList}</div>
+        <div className="row">{imageList}</div>
       </div>
     )
   }
